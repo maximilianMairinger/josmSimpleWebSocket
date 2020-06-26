dd676b3cc795d3f5dd3050f272dcb3918b77139f
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-26 15:32:41 +0200

    ws working

--- app/src/josmSimpleWebSocket.ts
+++ app/src/josmSimpleWebSocket.ts
@@ -1,5 +1,9 @@
-dependencyImports
-
 export default function() {
-  
+  const ws = new WebSocket("ws://127.0.0.1:8080")
+  ws.addEventListener("message", (q) => {
+    console.log("Message:", q.data)
+  })
+  ws.addEventListener("open", () => {
+    console.log("con")
+  })
 }
