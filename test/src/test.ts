5bb2dc9f4eb6664916bb5e54d55a6104fd005bf3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-26 23:53:00 +0200

    more to rollup build

--- test/src/test.ts
+++ test/src/test.ts
@@ -2,6 +2,9 @@ import josmSimpleWebSocket from "./../../app/src/josmSimpleWebSocket"
 import { Data } from "josm"
 
 
+console.log("qiuwhehiqhiuwhieu", 123);
+debugger
+
 (async () => {
   let data = await josmSimpleWebSocket("testClient", "127.0.0.1:8080", console.log) as Data
 
